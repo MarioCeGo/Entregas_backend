be372ee2c21a061c45d1f0e982203c5af43323a1
Mario Gonzalez <mariocego99@gmail.com>
2022-09-22 21:43:26 -0300

    Entrega desafio manejo de archivos

--- index.js
+++ index.js
@@ -1,38 +1,76 @@
-//Desafio clases - Mario Gonzalez
-class Usuario{
-    constructor(nombre, apellido, libros = [], mascotas = []){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.libros = libros;
-        this.mascotas = mascotas;
+//Desafio manejos de archivos - Mario Gonzalez
+const fs = require('fs')
+class Contenedor {
+    constructor(name) {
+        this.name = `./${name}.txt`
     }
-    getFullName(){
-        return `Nombre completo: ${this.nombre} ${this.apellido}`;
-    }
-    addMascota(mascota){
-        this.mascotas.push(mascota);
-    }
-    countMascotas(){
-        return  this.mascotas.length > 1 ? `El usuario tiene ${this.mascotas.length} mascotas` : `El usuario tiene una mascota`;
-    }
-    addBook(nombre, autor){
-        this.libros.push({nombre:nombre, autor:autor});
-    }
-    getBookNames(){
-        const bookNames = [];
-        for(const elem of this.libros){
-            bookNames.push(elem.nombre);
+    async save(prod) {
+        try {
+            try {
+                const data = await this.getAll();
+                data.length === 0 ? prod.id = 1 : prod.id = data[data.length - 1].id + 1;
+                data.push(prod);
+                await fs.promises.writeFile(this.name, JSON.stringify(data));
+            } catch (error) {
+                prod.id = 1
+                await fs.promises.writeFile(this.name, JSON.stringify([prod]));
+            }
+        } catch (error) {
+            console.log("No se pudo guardar correctamente")
         }
-        return bookNames
+    }
+
+    async getById(id) {
+        try {
+            const data = await this.getAll();
+            console.log(data.find(elem => elem.id === id))
+        } catch (error) {
+            console.log("No se pudo encontrar el elemento")
+        }
+    }
+    async getAll() {
+        try {
+            const data = await fs.promises.readFile(this.name, 'utf-8');
+            return (data.length > 0 ? JSON.parse(data) : []);
+        } catch (error) {
+
+        }
+    }
+    async deleteById(id) {
+        try {
+            const data = await this.getAll();
+            data.splice(data.indexOf(data.find(elem => elem.id === id)), 1)
+            await fs.promises.writeFile(this.name, JSON.stringify(data));
+            console.log("Elemento borrado correctamente")
+        } catch (error) {
+            console.log("No se pudo borrar correctamente");
+        }
+
     }
 }
-const usuario = new Usuario ("Elon", "Musk");
-usuario.addMascota("Perro");
-usuario.addMascota("Gato");
-usuario.addBook("El señor de las moscas", "William Golding");
-usuario.addBook("Fundacion", "Isaac Asimov");
+const contenedor = new Contenedor('productos');
 
-console.log(usuario.getFullName());
-console.log(usuario.getBookNames());
-console.log(usuario.countMascotas());
+// contenedor.save({
+//     title: 'Calculadora',
+//     price: 234.56,
+//     thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png',
+//   }
+// )
 
+// contenedor.save({
+//     title: 'Globo Terráqueo',
+//     price: 345.67,
+//     thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png',
+//   }
+// )
+
+// contenedor.save({
+//     title: 'Escuadra',
+//     price: 123.45,
+//     thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
+// }
+// )
+
+// contenedor.deleteById(4)
+
+// contenedor.getById(1)
