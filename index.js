9a4415e10b26cedf410842d7d6fd16c0bfdf9459
Mario Gonzalez <mariocego99@gmail.com>
2022-10-11 20:35:56 -0300

    Desafio API RESTful

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const fs = require('fs')
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-
+const { Router } = express;
 
 class Contenedor {
     constructor(name) {
@@ -25,21 +24,32 @@ class Contenedor {
             console.log("No se pudo guardar correctamente")
         }
     }
-
     async getById(id) {
         try {
             const data = await this.getAll();
-            console.log(data.find(elem => elem.id === id))
+            if (data.includes(data.find(elem => elem.id === id))) {
+                return data.find(elem => elem.id === id);
+            } else {
+                return { error: 'producto no encontrado' }
+
+            }
+
         } catch (error) {
-            console.log("No se pudo encontrar el elemento")
         }
+    }
+    async editById(id, { title, price, thumbnail }){
+        const data = await this.getAll();
+        const prod = data.find(elem => elem.id === Number(id));
+        prod.title = title;
+        prod.price = price;
+        prod.thumbnail = thumbnail;
+        await fs.promises.writeFile(this.name, JSON.stringify(data));
     }
     async getAll() {
         try {
             const data = await fs.promises.readFile(this.name, 'utf-8');
             return await (data.length > 0 ? JSON.parse(data) : []);
         } catch (error) {
-
         }
     }
     async deleteById(id) {
@@ -47,29 +57,48 @@ class Contenedor {
             const data = await this.getAll();
             data.splice(data.indexOf(data.find(elem => elem.id === id)), 1)
             await fs.promises.writeFile(this.name, JSON.stringify(data));
-            console.log("Elemento borrado correctamente")
+            return ("Elemento borrado correctamente")
         } catch (error) {
-            console.log("No se pudo borrar correctamente");
+            return ("No se pudo borrar correctamente");
         }
-
     }
-    async deleteAll(){
+    async deleteAll() {
         await fs.promises.writeFile(this.name, JSON.stringify([]));
     }
 }
 
 const contenedor = new Contenedor('productos');
 
-app.get('/productos', async(req, res) => {
+
+const apiRouter = Router();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+apiRouter.get('/productos', async (req, res) => {
     res.send(await contenedor.getAll());
 })
-app.get('/productoRandom', async (req, res) => {
-    const productos = await contenedor.getAll()
-    const prodRandom = Math.floor(Math.random() * (productos.length - 0 + 1) + 0);
-    res.send(productos[prodRandom]);
+apiRouter.get('/productos/:id', async (req, res) => {
+    res.send(await contenedor.getById(Number(req.params.id)))
 })
+apiRouter.post('/productos', async (req, res) => {
+    const { title, price, thumbnail } = req.body;
+    await contenedor.save({ title, price, thumbnail });
+    res.send({ saved: true });
+})
+apiRouter.put('/productos/:id', async (req, res) => {
+    const { title, price, thumbnail } = req.body;
+    await contenedor.editById(req.params.id, { title, price, thumbnail });
+    res.send({edited: true});
+})
+apiRouter.delete('/productos/:id', async (req, res) => {
+    res.send(await contenedor.deleteById(Number(req.params.id)));
+})
+app.use(express.static(__dirname + '/public'));
 
-const server = app.listen(PORT, ()=> console.log('Server is running'));
-server.on('error', err => console.log('error'))
+app.use('/API', apiRouter)
+app.listen(8080)
+
+// const server = app.listen(PORT, ()=> console.log('Server is running'));
+// server.on('error', err => console.log('error'))
 
 
